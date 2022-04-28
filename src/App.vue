53a96051393ce1ef91e1354f8c4d4aec07a32b4a
aluno <vagnercardozo2002@gmail.com>
2022-04-28 11:27:30 -0300

    Adicionando

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+import ListTasks from "./components/ListTasks.vue";
+export default {
+  components: { ListTasks },
+  methods: {
+    moveTask(origem, destino, task) {
+      this.etapas[destino].cards.push(task);
+    },
+  },
+  data: function () {
+    return {
+      etapas: {
+        toDo: {
+          name: "toDo",
+          titulo: "ToDO",
+          cards: [
+            { nome: "Refazer tudo", id: "1" },
+            { nome: "Refazer component", id: "2" },
+          ],
+          next: "done",
+        },
+        done: {
+          name: "done",
+          titulo: "Done",
+          cards: [
+            { nome: "Refazer backend", id: "3" },
+            { nome: "Corrigir bug component", id: "4" },
+          ],
+          next: "",
+          back: "toDo",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<template>
+  <ListTasks :tasks="etapas.toDo" @moveTask="moveTask" />
+  <ListTasks :tasks="etapas.done" @moveTask="moveTask" />
+</template>
+
+<style></style>
---
+++ src/components/ListTasks.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  props: ["tasks"],
+};
+</script>
+
+<template>
+  <ol>
+    {{
+      tasks.titulo
+    }}
+    <li v-for="task in tasks.cards" :key="task.id">
+      {{ task.nome }}
+      <input
+        v-if="tasks.next"
+        type="button"
+        value="Proximo"
+        @click="$emit('moveTask', tasks.name, tasks.next, task)"
+      />
+      <input
+        v-if="tasks.back"
+        type="button"
+        value="Anterior"
+        @click="$emit('moveTask', tasks.name, tasks.back, task)"
+      />
+    </li>
+  </ol>
+</template>
+
+<style></style>
