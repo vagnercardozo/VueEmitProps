901cc9eadcdab2436faa179b9dd6787a91183926
aluno <vagnercardozo2002@gmail.com>
2022-05-04 11:20:29 -0300

    feat: Adicionando o css para criar os cards

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,42 @@ export default {
   methods: {
     moveTask(origem, destino, task) {
       this.etapas[destino].cards.push(task);
+
+      const removeIndex = this.etapas[origem].cards
+        .map((task) => task.id)
+        .indexOf(task.id, 0);
+
+      this.etapas[origem].cards.splice(removeIndex, 1);
     },
   },
   data: function () {
     return {
       etapas: {
+        investigation: {
+          name: "investigation",
+          titulo: "Investigation",
+          cards: [
+            { nome: "Refazer tudo", id: "1" },
+            { nome: "Refazer component", id: "2" },
+          ],
+          next: "toDo",
+          back: "",
+        },
         toDo: {
           name: "toDo",
-          titulo: "ToDO",
+          titulo: "ToDo",
           cards: [
             { nome: "Refazer tudo", id: "1" },
             { nome: "Refazer component", id: "2" },
           ],
           next: "done",
+          back: "investigation",
         },
         done: {
           name: "done",
           titulo: "Done",
           cards: [
-            { nome: "Refazer backend", id: "3" },
+            { nome: "Refazer backend", id: "3"  },
             { nome: "Corrigir bug component", id: "4" },
           ],
           next: "",
@@ -36,8 +53,16 @@ export default {
 </script>
 
 <template>
-  <ListTasks :tasks="etapas.toDo" @moveTask="moveTask" />
-  <ListTasks :tasks="etapas.done" @moveTask="moveTask" />
+  <div class="container">
+    <ListTasks :tasks="etapas.investigation" @moveTask="moveTask" />
+    <ListTasks :tasks="etapas.toDo" @moveTask="moveTask" />
+    <ListTasks :tasks="etapas.done" @moveTask="moveTask" />
+  </div>
 </template>
 
-<style></style>
+<style>
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
--- src/components/ListTasks.vue
+++ src/components/ListTasks.vue
@@ -5,26 +5,46 @@ export default {
 </script>
 
 <template>
-  <ol>
-    {{
-      tasks.titulo
-    }}
-    <li v-for="task in tasks.cards" :key="task.id">
-      {{ task.nome }}
-      <input
-        v-if="tasks.next"
-        type="button"
-        value="Proximo"
-        @click="$emit('moveTask', tasks.name, tasks.next, task)"
-      />
-      <input
-        v-if="tasks.back"
-        type="button"
-        value="Anterior"
-        @click="$emit('moveTask', tasks.name, tasks.back, task)"
-      />
-    </li>
-  </ol>
+  <div class="etapas">
+    <ol>
+      <div class="titulo">
+        {{ tasks.titulo }}
+      </div>
+      <li v-for="task in tasks.cards" :key="task.id">
+        {{ task.nome }}
+        <input
+          v-if="tasks.next"
+          type="button"
+          value="Proximo"
+          @click="$emit('moveTask', tasks.name, tasks.next, task)"
+        />
+        <input
+          v-if="tasks.back"
+          type="button"
+          value="Anterior"
+          @click="$emit('moveTask', tasks.name, tasks.back, task)"
+        />
+      </li>
+    </ol>
+  </div>
 </template>
 
-<style></style>
+<style>
+.etapas {
+  width: 30%;
+  background-color: aquamarine;
+  justify-content: space-around;
+}
+.titulo {
+  text-align: center;
+  font-weight: bold;
+  margin-bottom: 25px;
+}
+li {
+  list-style-type: none;
+  width: 90%;
+  height: 200px;
+  background-color: aliceblue;
+  margin-bottom: 10px;
+}
+</style>
